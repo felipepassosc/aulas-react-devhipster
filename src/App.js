926e4daf07d87d01c654764dfc8af6838136c1fe
felipe.passos <felipe.passos@iv2.com.br>
2019-10-04 17:31:33 -0300

    att

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar'
+import Formulario from './Formulario';
+
+function App() {
+  return (
+    <div>
+     <Navbar/>
+     <Formulario/>
+     </div>
+  );
+}
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render(){
+        return (
+            <nav className="navbar navbar-light shadow p-3 mb-5 bg-primary rounded">
+            <a className="navbar-brand text-white" href="#">Meu sistema</a>
+          </nav>
+        )
+    }
+}
+
+export default Navbar;
+
